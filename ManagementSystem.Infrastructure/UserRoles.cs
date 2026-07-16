6f7b0573b1adc5527bae94d5e0327c34cc551195
Euden Ajdini <146646290+Euden8@users.noreply.github.com>
2026-07-16 17:05:52 +0200

    chore: add .gitignore and untrack build files

---
+++ ManagementSystem.Infrastructure/UserRoles.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Infrastructure
+{
+    public class UserRoles
+    {
+        public const string SuperAdmin = "SuperAdmin";
+    public const string Admin = "Admin";
+    public const string Manager = "Manager";
+    public const string Viewer = "Viewer";
+    }
+}
